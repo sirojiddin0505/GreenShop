a28fc29db91c9e9deab58f158aec562ccc2b0f22
sirojiddin0505 <sirojiddintohirjanov6@gmail.com>
2025-04-22 18:54:15 +0500

    hero.css

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,6 +1,7 @@
 import './hero.css'
 import hero1 from '../hero/images/hero1.svg'
 import hero2 from '../hero/images/hero2.png'
+import boxs1 from '../hero/images/boxs1.svg'
 
 const hero = ()=>{
     return (
@@ -41,7 +42,67 @@ const hero = ()=>{
                         <img src={hero1} alt="" className='left__img'/>
                     </div>
                 </div>
-                <div className="hero__right"></div>
+                <div className="hero__right">
+                    <div className="right__start">
+                        <h2 className="right__subtitle">All Plants</h2>
+                        <h2 className="right__subtitle">New Arrivals</h2>
+                        <h2 className="right__subtitle">Sale</h2>
+                        <select name="change">
+                            <option value="#" disabled >Default sorting</option>
+                            <option value="#">Sort 1</option>
+                            <option value="#">Sort 2</option>
+                            <option value="#">Sort 3</option>
+                            <option value="#">Sort 4</option>
+                        </select>
+                    </div>
+                    <div className="right__end">
+                        <div className="hero__boxs">
+                            <img src={boxs1} alt="" className='hero__pic' />
+                            <h3 className="right__title">Barberton Daisy</h3>
+                            <p className="right__text">$119.00</p>
+                        </div>
+                        <div className="hero__boxs">
+                            <img src={boxs1} alt="" className='hero__pic'/>
+                            <h3 className="right__title">Angel Wing Begonia</h3>
+                            <p className="right__text">$116.00</p>
+                        </div>
+                        <div className="hero__boxs">
+                            <img src={boxs1} alt="" className='hero__pic'/>
+                            <h3 className="right__title">African Violet</h3>
+                            <p className="right__text">$199.00  <s>salom</s></p>
+                        </div>
+                        <div className="hero__boxs">
+                            <img src={boxs1} alt="" className='hero__pic'/>
+                            <h3 className="right__title">Beach Spider Lily</h3>
+                            <p className="right__text">$129.00</p>
+                        </div>
+                        <div className="hero__boxs">
+                            <img src={boxs1} alt="" className='hero__pic'/>
+                            <h3 className="right__title">Blushing Bromeliad</h3>
+                            <p className="right__text">$99.00</p>
+                        </div>
+                        <div className="hero__boxs">
+                            <img src={boxs1} alt="" className='hero__pic'/>
+                            <h3 className="right__title">Aluminum Plant</h3>
+                            <p className="right__text">$79.00</p>
+                        </div>
+                        <div className="hero__boxs">
+                            <img src={boxs1} alt="" className='hero__pic' />
+                            <h3 className="right__title">Bird's Nest Fern</h3>
+                            <p className="right__text">$69.00</p>
+                        </div>
+                        <div className="hero__boxs">
+                            <img src={boxs1} alt="" className='hero__pic'/>
+                            <h3 className="right__title">Broadleaf Lady Palm</h3>
+                            <p className="right__text">$139.00</p>
+                        </div>
+                        <div className="hero__boxs">
+                            <img src={boxs1} alt="" className='hero__pic'/>
+                            <h3 className="right__title">Chinese Evergreen</h3>
+                            <p className="right__text">$39.00</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
