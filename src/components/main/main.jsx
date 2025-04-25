7f6bd63535d6e877a82b575b81e3fc0a6201bc01
sirojiddin0505 <sirojiddintohirjanov6@gmail.com>
2025-04-25 10:49:43 +0500

    commit

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -11,11 +11,17 @@ const main = ()=>{
                     <img src={main1} alt="" className="main__img" />
                     <div className="main__box">
                         <h1 className="main__title">SUMMER CACTUS $&  SUCCULENTS</h1>
-                        <p className="main__text"></p>
+                        <p className="main__text">We are an online plant shop offering a wide range of cheap and trendy plants</p>
+                        <button className="main__btn">Find More</button>
                     </div>
                 </div>
                 <div className="main__right">
                 <img src={main2} alt="" className="main__img" />
+                <div className="main__box">
+                    <h1 className="main__title">Styling Trends & much more</h1>
+                    <p className="main__text">We are an online plant shop offering a wide range of cheap and trendy plants</p>
+                    <button className="main__btn">Find More</button>
+                </div>
                 </div>
             </div>
         </main>
